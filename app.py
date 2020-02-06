3f3af62b50d39c8bc8d05ab6571eb22f0b4eba48
jordanhuusi@gmail.com <jordanhuusi@gmail.com>
2020-02-05 19:39:07 -0800

    Migration update: added columns to Venue table.

--- app.py
+++ app.py
@@ -34,15 +34,37 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String())
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    image_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    '''
+    past shows count will be queried when the page is requested
+        count               -> Integer
+
+    upcoming shows count will be queried when the page is requested
+        count               -> Integer
+
+    past shows data will be queried when the page is requested
+        artist_id            -> Integer
+        artist_name          -> String
+        artist_image_link    -> String
+        start_time          -> String
+
+    upcoming shows will be queried when the page is requested
+        artist_id            -> Integer
+        artist_name          -> String
+        artist_image_link    -> String
+        start_time          -> String
+    '''
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -78,8 +100,6 @@ class Artist(db.Model):
         venue_image_link    -> String
         start_time          -> String
     '''
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
